1990de7a40669ab618d448d14eddeff5be58cb01
Albin-Olopo <albin.alex@olopo.app>
2024-09-06 09:59:13 +0530

    testing-update

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT || 5000,
     BIGCHAINDB_URL: process.env.BIGCHAINDB_URL || 'http://localhost:9984/api/v1/',
-    BROKERS: (process.env.BROKERS || 'localhost:9092,localhost:9093,localhost:9094').split(','),
+    BROKERS: (process.env.BROKERS || 'localhost:9092').split(','),
     CONSUMER_GROUP_ID: process.env.CONSUMER_GROUP_ID || 'GROUP1',
     CONSUMER_TOPIC: process.env.CONSUMER_TOPIC || 'TOPIC1',
     PRODUCER_TOPIC: process.env.PRODUCER_TOPIC || 'TOPIC2'
